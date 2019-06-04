daa554867336a5b79862edc2b604af463388ae24
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-04 22:15:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_hash = {
-    "car" => "Ford Escape"
-    "dog" => "Rudy"
+    "car" => "Ford Escape",
+    "dog" => "Rudy",
     "town" => "Fuquay-Varina"
   }
 
@@ -14,9 +14,9 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   the_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 2
-    "oil paintings" => 3
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3,
   }
 
 end
