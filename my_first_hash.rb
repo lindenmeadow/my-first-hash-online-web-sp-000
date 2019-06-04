4d953b78cef56c680cd07610bfc074459f7e18c2
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-04 22:14:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {
+    "car" => "Ford Escape"
+    "dog" => "Rudy"
+    "town" => "Fuquay-Varina"
+  }
   
 end
 
@@ -21,7 +25,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  
 end
 
 def adding
