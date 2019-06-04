13d933a995c2151b2b82945e214616ed227e4276
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-04 22:20:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,7 +29,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_maifest["oil paintings"]
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -39,11 +39,11 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest
 
 
   # return the shipping_manifest hash below
